c264cb48f960632935c22890d33957eddaf3dd48
taufiq aditya putra <taufiqadityaputra@gmail.com>
2025-09-10 15:32:16 +0700

    pesan bebas

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { defineConfig } from "vite"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "https://gifporto.github.io/newport",
+  base: "/newport/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
